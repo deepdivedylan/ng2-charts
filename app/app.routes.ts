bc6a9cc41bb128f561eef842494bbf60363dde21
Dylan McDonald <dmcdonald21@cnm.edu>
2017-02-22 21:39:15 -0700

    added pie route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,9 +1,11 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
+import {PieComponent} from "./components/pie-component";
 
-export const allAppComponents = [HomeComponent];
+export const allAppComponents = [HomeComponent, PieComponent];
 
 export const routes: Routes = [
+	{path: "pie", component: PieComponent},
 	{path: "", component: HomeComponent}
 ];
 
