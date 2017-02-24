a318c4b5d1f406af54a79387b6596bf3f714363d
Dylan McDonald <dmcdonald21@cnm.edu>
2017-02-23 20:21:30 -0700

    added line route

--- app/app.routes.ts
+++ app/app.routes.ts
@@ -1,10 +1,12 @@
 import {RouterModule, Routes} from "@angular/router";
 import {HomeComponent} from "./components/home-component";
+import {LineComponent} from "./components/line-component";
 import {PieComponent} from "./components/pie-component";
 
-export const allAppComponents = [HomeComponent, PieComponent];
+export const allAppComponents = [HomeComponent, LineComponent, PieComponent];
 
 export const routes: Routes = [
+	{path: "line", component: LineComponent},
 	{path: "pie", component: PieComponent},
 	{path: "", component: HomeComponent}
 ];
